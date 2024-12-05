a726ba96bb7e761e528108135c86a9f813ecc63f
sooj1n <sujin19371@gmail.com>
2024-12-05 17:45:52 +0900

    airflow 진행 중

--- docker/airflow/dags/code/parkinginfo.py
+++ docker/airflow/dags/code/parkinginfo.py
@@ -1,6 +1,7 @@
 import os
 import json
 import csv
+import pandas as pd
 
 def fun_api_call():
     import json
@@ -45,25 +46,28 @@ def fun_branch(*args, **kwargs):
     dir_path = f"/opt/airflow/data/"
     filename = kwargs.get('filename')
     file_path = os.path.join(dir_path, filename)
+    
 
     print(f"*****************  {file_path}")
-    if os.path.exists(file_path): #폴더가 존재하면
+    if os.path.exists(file_path):
+        print("파일이 존재합니다.") 
         if filename=='edata.json':
             return 'remove_extract_log'  
         else:
             return 'remove_transform_log'
-    
-    else:
-        print("폴더가 존재하지 않습니다.")
+        
+    else: #파일이 없음
+        print("파일이 존재하지 않습니다.")
         if filename=='edata.json':
             return 'extract_empty'  
         else:
             return 'transform_empty'
 
+
 def fun_remove_log(filename):
-    # path = "/opt/airflow/Edata/"
-    dir_path = "opt/airflow/data"
+    dir_path = "/opt/airflow/data/"
     file_path = os.path.join(dir_path, filename)
+  
     try:
         os.remove(file_path)
         print(f"{file_path} 파일이 삭제되었습니다.")
@@ -107,70 +111,188 @@ def fun_save_log():
 
         if First:
             # totalcnt = data['totalCount'] 
-            totalcnt= 5
+            totalcnt= 30
             First=False
    
         currentcnt+=int(data['numOfRows'])
         pagenum+=1
 
-    # 파일에 내용 작성
-    with open(file_path, 'w', encoding='utf-8') as f:
-        json.dump(json_data, f, ensure_ascii=False, indent=4)
+    try:
+        with open(file_path, 'w', encoding='utf-8') as f:
+            json.dump(json_data, f, ensure_ascii=False, indent=4)
+        print("파일에 내용이 성공적으로 작성되었습니다.")
+    except Exception as e:
+        print(f"파일 작성 중 오류가 발생했습니다: {e}")
 
 
 def fun_trans():
     # log 읽기
-    dir_path = "/opt/airflow/Edata/"
-    filename = "data.json"
-    csv_filename = 'data.csv'
-    json_file_path = os.path.join(dir_path, filename)
+    dir_path = "/opt/airflow/data/"
+    json_filename = "edata.json"
+    csv_filename = "tdata.csv"
+    json_file_path = os.path.join(dir_path, json_filename)
     csv_file_path = os.path.join(dir_path,csv_filename)
 
     try:
         if os.path.exists(json_file_path):
             with open(json_file_path, 'r', encoding='utf-8') as file:
-                data = json.load(file)  
-                
+                data = json.load(file)         
         else:
             print(f"경로에 파일이 존재하지 않습니다.")
     except Exception as e:
         print(f"파읽 읽는 중 오류 발생: {e}")
     
+
+
     header = ['park_id', 'park_nm', 'park_addr', 'park_la', 'park_lo', 'page_no']
-    if not os.path.exists(csv_file_path) or os.path.getsize(csv_file_path) == 0:
+    if (not os.path.exists(csv_file_path)) or (os.path.getsize(csv_file_path) == 0):
         with open(csv_file_path, mode='w', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(header) 
             print("헤더가 추가되었습니다.")
 
-    # 필요한 것만 뽑아내기
+
     for d in data:
         print("*"*30)
         print(d)
         pagenum = d['pageNo']
         for prk_data in d['PrkSttusInfo'] :
-            prk_list =[]
+            
             parkid = prk_data['prk_center_id']
             parknm = prk_data['prk_plce_nm']
             parkaddr = prk_data['prk_plce_adres']
             parkla = prk_data['prk_plce_entrc_la']
             parklo = prk_data['prk_plce_entrc_lo']
-            prk_list.append([parkid, parknm, parkaddr, parkla, parklo, pagenum])
+            
 
-            with open(csv_file_path, mode='a') as file:
-                # 리스트를 쉼표로 구분한 문자열로 변환
-                row = ','.join(map(str, prk_list)) + '\n'
+            row = f"{parkid},{parknm},{parkaddr},{parkla},{parklo},{pagenum}\n"
+        
+            with open(csv_file_path, mode='a', encoding='utf-8') as file:
                 file.write(row)
 
 
-        
 
-def fun_conn():
-    print(f"fun_conn")
+def fun_2parquet():
+    import shutil
+
+    dir_path = "/opt/airflow/data/"
+    file_name = "tdata.csv"
+    path = os.path.join(dir_path, file_name)
+    df = pd.read_csv(path)
+    print("^"*1000)
+    print(df.isnull().sum())
+    df = df.fillna('')
+    grouped = df.groupby('page_no')
+
+    p_dir_path = f"{dir_path}parquet"
+    #이미 존재하면 먼저 삭제하고
+    try:
+        if os.path.exists(p_dir_path): 
+            shutil.rmtree(p_dir_path)
+    except Exception as e:
+        print(f"디렉토리 생성 중 오류 발생: {e}")
+
+    # parquet 폴더 생성
+    try:
+        os.makedirs(p_dir_path)
+    except Exception as e:
+        print(f"디렉토리 생성 중 오류 발생: {e}")
+    
+
+    print(df['park_id'][2:-1])
+    print(df['park_nm'][1:-1])
+    print(df['park_addr'][1:-1])
+    print(df['park_la'][1:-1])
+    print(df['park_lo'][1:-1])
+    print(df['page_no'].astype('int'))
+
+
+    # 각 page_no별로 데이터를 Parquet로 저장
+    for page_no, group in grouped:
+        # 각 page_no에 대해 parquet 파일로 저장    
+        output_file = f"{dir_path}/parquet/parkinginfo_{page_no}.parquet"
+        group.to_parquet(output_file, index=False)
+        print(f"Page {page_no} 데이터가 {output_file}로 저장되었습니다.")
+
+
 
 def fun_load():
-    print(f"fun_load")
+    import glob
+    from mysql.connector import Error
+    import mysql.connector
 
+    try:
+        # MySQL 연결 설정
+        passwd = os.getenv('DB_PW')  # 환경 변수에서 DB 비밀번호 가져오기
+
+        conn = mysql.connector.connect(
+            host="parkingissue_database",
+            port=3306,
+            user="root",
+            password=passwd,
+            database="parkingissue"
+        )
+
+        # 연결 성공 여부 확인
+        if conn.is_connected():
+            print("MySQL 데이터베이스에 성공적으로 연결되었습니다.")
+            
+    except Error as e:
+        print(f"Error while connecting to MySQL: {e}")
+        return None
+    
+    try: 
+        cursor = conn.cursor()
+        # DELETE 쿼리 실행
+        delete_query = "DELETE FROM parkingarea_info;"
+        cursor.execute(delete_query)
+        print("데이터가 삭제되었습니다.")
+        
+        # AUTO_INCREMENT 초기화 쿼리 실행
+        alter_query = "ALTER TABLE parkingarea_info AUTO_INCREMENT = 1;"
+        cursor.execute(alter_query)
+        print("AUTO_INCREMENT가 초기화되었습니다.")
+
+    except Error as e:
+        print(f"테이블 초기화 오류 발생!!  {e}")
+
+    folder_path = '/opt/airflow/data/parquet'
+    parquet_files = glob.glob(os.path.join(folder_path, "*.parquet"))
+    parquet_files.sort(key=lambda x: int(os.path.basename(x).split('_')[1].split('.')[0]))
+
+    for file in parquet_files:
+        try:
+            # 파일 존재 여부 확인
+            if os.path.exists(file) and os.path.isfile(file):
+                # parquet 파일 읽기
+                df = pd.read_parquet(file)
+                print(f"파일명: {os.path.basename(file)}")
+                print(df)  
+                print("-" * 50)
+
+                # 한 번에 INSERT 하기
+                if not df.empty:
+                    insert_query = """
+                    INSERT INTO parkingarea_info (park_id, park_nm, park_addr, park_la, park_lo, page_no) VALUES (%s, %s, %s, %s, %s, %s)
+                    """
+                    # DataFrame을 리스트로 변환
+                    data = [tuple(x) for x in df[['park_id', 'park_nm', 'park_addr', 'park_la', 'park_lo', 'page_no']].values]
+                    cursor = conn.cursor()
+                    cursor.executemany(insert_query, data)  # 여러 행을 한 번에 삽입
+                    conn.commit()
+                    print(f"{len(data)}개의 데이터가 삽입되었습니다.")
+                else:
+                    print("DataFrame이 비어 있습니다.")
+            else:
+                print(f"파일이 존재하지 않거나 잘못된 경로입니다: {file}")
+        except Exception as e:
+            # 오류 발생 시 처리
+            print(f"파일 {os.path.basename(file)}을(를) 읽는 중 오류 발생: {e}")
+
+    if conn.is_connected():
+        cursor.close()
+        conn.close()
+        print("DB connection closed.")
 
 
 def call(pagenum,API_KEY):
--- docker/airflow/dags/example.py
+++ docker/airflow/dags/example.py
@@ -3,7 +3,7 @@ sys.path.insert(0, '/opt/airflow/dags/code')
 
 from parkinginfo import (
     fun_api_call, fun_branch, fun_remove_log, fun_save_log, 
-    fun_trans, fun_conn, fun_load
+    fun_trans, fun_load,fun_2parquet
 )
 from datetime import datetime
 from airflow.models.dag import DAG
@@ -19,6 +19,16 @@ def create_python_operator(task_id, python_callable,t_rule='all_success'):
         provide_context=True,
         dag=dag,
         trigger_rule=t_rule,
+    )
+
+def create_python_operator_op(task_id, python_callable, filename):
+    """PythonOperator 생성 함수"""
+    return PythonOperator(
+        task_id=task_id,
+        python_callable=python_callable,
+        provide_context=True,
+        dag=dag,
+        op_kwargs={'filename': filename}
     )
 
 def create_branch_operator(task_id, python_callable, filename):
@@ -44,10 +54,10 @@ with DAG(
 
     # 첫 번째 줄 - API 호출 및 추출 작업
     api_call = create_python_operator('api_call', fun_api_call)
-    extract_branch = create_branch_operator('extract_branch', fun_branch, "extract.csv")
+    extract_branch = create_branch_operator('extract_branch', fun_branch, "edata.json")
     extract_empty = EmptyOperator(task_id="extract_empty")
    
-    remove_extract_log = create_python_operator('remove_extract_log', fun_remove_log)
+    remove_extract_log = create_python_operator_op('remove_extract_log', fun_remove_log, "edata.json")
     save_extract_log = create_python_operator('save_extract_log', fun_save_log,'all_done')
 
     # 첫 번째 줄 작업 순서: start -> api_call_check -> extract_data_branch -> [remove_extract_log, save_extract_log]
@@ -56,24 +66,19 @@ with DAG(
     remove_extract_log >> save_extract_log
 
     # 두 번째 줄 - 변환 및 적재 작업
-    transform = create_python_operator('transform', fun_trans)
+    transform = create_python_operator('transform', fun_trans,'all_done')
 
-    transform_branch = create_branch_operator('transform_branch', fun_branch, "transform.csv")
+    transform_branch = create_branch_operator('transform_branch', fun_branch, "tdata.csv")
     transform_empty = EmptyOperator(task_id="transform_empty")
-    remove_transform_log = BashOperator(
-    task_id="remove_transform_log",
-    bash_command="""
-        echo remove_transform_log !!!!
-    """
-    )
-    save_transform_log = create_python_operator('save_transform_log', fun_save_log,'all_done')
+    remove_transform_log = create_python_operator_op('remove_transform_log', fun_remove_log, "tdata.csv")
 
-    connect_db = create_python_operator('connect_db', fun_conn)
+    save_transform_log = create_python_operator('save_transform_log', fun_2parquet)
+
     load_data = create_python_operator('load_data', fun_load)
 
     # 두 번째 줄 작업 순서: save_extract_log -> data_transformation -> transform_data_branch -> [remove_transform_log, save_transform_log] -> connect_to_db -> load_data_to_db -> end
-    save_extract_log >> transform >> transform_branch
-    transform_branch >> [remove_transform_log, transform_empty]
-    remove_transform_log >> save_transform_log
+    # save_extract_log >> transform >> transform_branch
+    save_extract_log >> transform_branch >> [remove_transform_log, transform_empty]
+    remove_transform_log >> transform >> save_transform_log
 
-    save_transform_log >> connect_db >> load_data >> end
+    save_transform_log >> load_data >> end
