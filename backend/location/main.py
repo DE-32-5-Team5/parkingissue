23fc0134e448a2d4fd397e43217bea61037c9b47
tbongkim03 <tbongkim03@gmial.com>
2024-12-13 12:46:58 +0900

    main.py 3endpoint success

--- backend/location/main.py
+++ backend/location/main.py
@@ -4,7 +4,7 @@ from kafka_producer import send_to_kafka
 import requests
 
 
-app = FastAPI()
+app = FastAPI(docs_url='/api/docs', openapi_url='/api/openapi.json')
 
 @app.get("/")
 def read_root():
@@ -12,8 +12,9 @@ def read_root():
 
 @app.post("/api/location")
 async def create_item(location: Location):
-    send_to_kafka({"lo": location.lo, "la": location.la})
-    # return {"lo": location.lo, "la": location.la}
+    return "hello"
+    #send_to_kafka({"lo": location.lo, "la": location.la})
+    #return {"lo": location.lo, "la": location.la}
 
 #스파크에서 id, lo, la가 json형식으로 올 거임. 그걸 받아서 다시 front에 post형식으로 보내기
 stored_data={}
@@ -22,10 +23,10 @@ async def receive_location(sparkdata: FromSpark):
     print(f"Received location: {sparkdata}")
     if not sparkdata:
         raise HTTPException(status_code=400, detail="Empty JSON data")
-    
-    stored_data.clear()  
+
+    stored_data.clear()
     stored_data.update(sparkdata)
-    # return {"message": "Data received and stored successfully.", "stored_data": stored_data}
+    return {"message": "Data received and stored successfully.", "stored_data": stored_data}
 
 
 
@@ -35,5 +36,5 @@ async def receive_location(sparkdata: FromSpark):
 async def frontget():
     if not stored_data:
         return {"message": "데이터가 없습니다."}
-    
+
     return {"stored_data": stored_data}
