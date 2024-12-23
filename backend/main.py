baebfaf6066e7a24a698c747c010d6d1d99fcd82
WhiteCapella <whitecapella.official@gmail.com>
2024-12-23 17:01:05 +0900

    phase 2

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 from routers import login, register, location, hotplace, mypage
 from routers import event
 
-app = FastAPI()
-
-# CORS 설정
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  
-    allow_credentials=True,
-    allow_methods=["*"], 
-    allow_headers=["*"],  
-)
+app = FastAPI(docs_url='/api/docs', openapi_url='/api/openapi.json')
 
 app.include_router(login.router)
 app.include_router(register.router)
