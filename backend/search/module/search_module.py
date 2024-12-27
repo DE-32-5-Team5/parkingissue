b54d9394a2a48d023b203909dc129171f0019e31
Kimwonjoon <kimpass189@gmail.com>
2024-12-27 12:34:11 +0900

    search backend 최신화

---
+++ backend/search/module/search_module.py
@@ -0,0 +1,32 @@
+import pymysql.cursors
+from db import search_db
+
+def searchParkDB(word):
+    connection = search_db()
+
+    with connection:
+        with connection.cursor() as cursor:
+            sql = f"""
+            SELECT park_id, park_nm, park_addr, park_la, park_lo
+            FROM parkingarea_info
+            WHERE park_nm LIKE '%{word}%'
+            LIMIT 1;
+            """
+            cursor.execute(sql,)
+            result = cursor.fetchone()
+            return result
+
+def searchHotDB(word):
+    connection = search_db()
+
+    with connection:
+        with connection.cursor() as cursor:
+            sql = f"""
+            SELECT contentid, mapx, mapy
+            FROM festival_info
+            WHERE title LIKE '%{word}%'
+            LIMIT 1;
+            """
+            cursor.execute(sql,)
+            result = cursor.fetchone()
+            return result
